08b11fbb308b599ce1a488b4db050ececa32004c
mushari44 <meeshoo-010@hotmail.com>
2024-06-20 16:15:17 +0300

    port changed

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -114,7 +114,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
