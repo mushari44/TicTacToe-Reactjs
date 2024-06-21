73cbb61dc9abd2c08a67fc2b137ab91c9cc52018
mushari44 <meeshoo-010@hotmail.com>
2024-06-21 19:15:30 +0300

    heruko deployment update

--- FrontEnd/mygame/src/components/index.jsx
+++ FrontEnd/mygame/src/components/index.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { io } from "socket.io-client";
 import "./styles.css";
 
-const socket = io("https://tic-tac-toe-server1.vercel.app/");
+const socket = io("https://tic-tac-toe-server1-a977e7db17f2.herokuapp.com/");
 
 function Square({ value, onClick }) {
   return (
@@ -24,7 +24,7 @@ export default function TicTacToe() {
     async function fetchGame() {
       try {
         const response = await axios.get(
-          "https://tic-tac-toe-server1.vercel.app/"
+          "https://tic-tac-toe-server1-a977e7db17f2.herokuapp.com/"
         );
         const game = response.data[0];
         if (game) {
@@ -84,12 +84,15 @@ export default function TicTacToe() {
       setIsXTurn(newTurn);
 
       try {
-        await axios.put("https://tic-tac-toe-server1.vercel.app/update-game", {
-          id: gameId,
-          squares: newSquares,
-          isXTurn: newTurn,
-          isGameOver: !!getWinner(newSquares) || !newSquares.includes(""),
-        });
+        await axios.put(
+          "https://tic-tac-toe-server1-a977e7db17f2.herokuapp.com/update-game",
+          {
+            id: gameId,
+            squares: newSquares,
+            isXTurn: newTurn,
+            isGameOver: !!getWinner(newSquares) || !newSquares.includes(""),
+          }
+        );
       } catch (error) {
         console.log("Error updating game:", error);
       }
@@ -124,9 +127,12 @@ export default function TicTacToe() {
 
   async function handleRestart() {
     try {
-      await axios.put("https://tic-tac-toe-server1.vercel.app/restart-game", {
-        id: gameId,
-      });
+      await axios.put(
+        "https://tic-tac-toe-server1-a977e7db17f2.herokuapp.com/restart-game",
+        {
+          id: gameId,
+        }
+      );
       setIsXTurn(true);
       setGameOver(false);
       setMessage("Next turn is X");
