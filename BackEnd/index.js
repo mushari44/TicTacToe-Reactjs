c730d8353339154901487c30aa2524fd0de222a5
mushari44 <meeshoo-010@hotmail.com>
2024-06-21 19:18:45 +0300

    heruko deployment update

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -12,8 +12,6 @@ const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:3000", // Your frontend local development URL
   "https://tictactoe.mushari-alothman.uk/", // Your frontend deployment URL
-  "https://mushari-tic-tac-toe.vercel.app/",
-  "https://tic-tac-toe-server1-a977e7db17f2.herokuapp.com/",
 ];
 
 const io = new Server(server, {
