e26426a6577fc4ac5676b3a373fa315814868ae9
mushari44 <meeshoo-010@hotmail.com>
2024-06-21 16:54:06 +0300

    changed cors methods

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,15 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
-const { log } = require("console");
-const { join } = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["Access-Control-Allow-Methods", "GET", "POST", "PUT"],
   },
 });
 
