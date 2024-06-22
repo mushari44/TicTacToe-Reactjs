0f26a7ac7c74d0a0ede07dddbbd150347f8767c6
mushari44 <meeshoo-010@hotmail.com>
2024-06-22 17:27:19 +0300

    player move

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,41 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "HEAD"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const mongoURI =
+  "mongodb+srv://musharizh56:admin@cluster0.clvs4os.mongodb.net/TicTacToe";
+
+mongoose
+  .connect(process.env.MONGODB_URI || mongoURI)
+  .then(async () => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB: ", error);
+  });
 
 const gameSchema = new mongoose.Schema({
   squares: {
@@ -62,8 +99,21 @@ app.put("/make-move", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+const Game = mongoose.model("Game", gameSchema);
 
-// Example of restarting game
+app.get("/", async (req, res) => {
+  try {
+    let game = await Game.findOne();
+    if (!game) {
+      game = new Game();
+      await game.save();
+    }
+    res.status(200).json([game]);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching game" });
+  }
+});
+
 app.put("/restart-game", async (req, res) => {
   const { gameId } = req.body;
   try {
@@ -86,4 +136,27 @@ app.put("/restart-game", async (req, res) => {
     console.error("Error restarting game:", error);
     res.status(500).json({ error: "Server error" });
   }
+});
+app.put("/update-game", async (req, res) => {
+  const { id, squares, isXTurn, isGameOver } = req.body;
+  try {
+    const game = await Game.findById(id);
+    if (game) {
+      game.squares = squares;
+      game.isXTurn = isXTurn;
+      game.isGameOver = isGameOver;
+      await game.save();
+      io.emit("gameUpdated", game); // Emitting the game update event
+      res.status(200).json(game);
+    } else {
+      res.status(404).json({ error: "Game not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error updating game" });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
